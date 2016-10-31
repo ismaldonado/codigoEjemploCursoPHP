b401b6dcf68ac4026c77ef88804658d6fc2047bc
Pakito <paco.gomez.arnal@gmail.com>
2016-10-31 07:47:46 +0100

    Estructura for

---
+++ estructuraFor/ejemplo01.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Ejemplo 1 de FOR</title>
+  </head>
+  <body>
+    <?php
+    //Contar del 1 al 10
+    for ($i=1; $i < 11; $i++) {
+      echo $i."<br>";
+    }
+    echo "Fuera del bucle for <br>";
+    echo $i;
+     ?>
+  </body>
+</html>
